bba0f7bf0d74b3162dfac9573b6e2b28c18ce06c
Realwale <Olawale@22_>
2023-04-21 20:14:25 +0100

    Initial

--- src/main/java/net/walecode/restfulapiusermanagementcrud/dto/UserDTO.java
+++ src/main/java/net/walecode/restfulapiusermanagementcrud/dto/UserDTO.java
@@ -1,14 +1,10 @@
 package net.walecode.restfulapiusermanagementcrud.dto;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 @Setter
 @Getter
